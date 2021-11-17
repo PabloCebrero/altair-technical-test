101cf2ffa7bf5edf1a6c61610c72a31e7e45bdd4
Pablo Cebrero <pabcebrod@gmail.com>
2021-11-17 19:37:01 +0100

    Add api log
    A log has been added when starting the API

--- cmd/main.go
+++ cmd/main.go
@@ -1,11 +1,14 @@
 package main
 
 import (
+	"fmt"
 	"github.com/altair-tecnical-test/src/server"
 	"log"
 	"net/http"
 )
 func main() {
 	s := server.New()
+
+	fmt.Printf("Listening port 8080...")
 	log.Fatal(http.ListenAndServe(":8080", s.Router()))
 }
